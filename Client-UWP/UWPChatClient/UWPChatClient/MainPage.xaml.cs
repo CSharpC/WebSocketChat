67cbed07c2d4f83f0af3e1cd7e2cedee87eb0624
Reyth3 <mrreynevan2@gmail.com>
2017-02-26 13:11:52 +0100

    Added UWP Client Project

---
+++ Client-UWP/UWPChatClient/UWPChatClient/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPChatClient.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPChatClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NicknameChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender != null)
+            {
+                var t = ((TextBox)sender).Text;
+                if (t.Length > 4) connectButton.IsEnabled = true;
+                else connectButton.IsEnabled = false;
+            }
+        }
+
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            var u = new User(nicknameBox.Text);
+            var json = JsonConvert.SerializeObject(u, Formatting.Indented);
+        }
+    }
+}
---
+++ Client-UWP/UWPChatClient/UWPChatClient/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPChatClient.Models
+{
+    public class User
+    {
+        public User() { }
+        public User(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("nickname")]
+        public string Name { get; set; }
+    }
+}
